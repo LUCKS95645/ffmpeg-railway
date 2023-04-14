09bc4053e8b19a341eff224471bdb328e21c3876
feliperosenek <feliperosenek@gmail.com>
2023-04-14 16:14:25 -0300

    deploy

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-const { exec } = require('child_process');
-
-exec('ffmpeg -loop 1 -i capa.jpg -i output.mp3 -c:a copy -c:v libx264 -preset fast -crf 10 -shortest youtube_video.mp4', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-});
+import { path as ffmpeg } from '@ffmpeg-installer/ffmpeg';
+import { exec } from 'child_process';
+exec(`${ffmpeg} -loop 1 -i capa.jpg -i output.mp3 -c:a copy -c:v libx264 -preset fast -crf 10 -shortest youtube_video.mp4`)
